fb2ad9d94f6b6befec7fdce0a29898f48a86de1f
Eduardo Esteves <40532597+eduuesteves@users.noreply.github.com>
2025-09-17 16:03:50 +0000

    adicionado nova frase no arquivo index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Curso.dev</h1>
+    return (
+        <>
+        <h1>Curso.dev</h1>
+        <p>Fiz o deploy na vercel e estou testando comandos do Git.</p>
+        </>
+    )
 }
 
 export default Home;
