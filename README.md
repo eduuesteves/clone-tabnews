4bcb2e2fce15441cd1f78cc2aa4971d51b96bc7d
Eduardo Esteves <40532597+eduuesteves@users.noreply.github.com>
2025-09-20 18:28:46 +0000

    correção da estilização do código com prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto para aprimorar a lógica de programação
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 function Home() {
-    return (
-        <>
-        <h1>Curso.dev</h1>
-        <p>Fiz o deploy na vercel e estou testando comandos do Git.</p>
-        <p>🌚</p>
-        </>
-    )
+  return (
+    <>
+      <h1>Curso.dev</h1>
+      <p>Fiz o deploy na vercel e estou testando comandos do Git.</p>
+      <p>🌚</p>
+    </>
+  );
 }
 
 export default Home;
